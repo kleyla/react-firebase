91364f584896a0cd654cdbe59fd2d02007fd5989
kleyla <kleyla01@live.com>
2020-11-01 10:06:55 -0400

    Updates

---
+++ precache-manifest.7e7669780ce06948c0d3b8292cdae539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dead70e6b2fd6d7383183ca2a9271f6",
+    "url": "/react-firebase/index.html"
+  },
+  {
+    "revision": "27cfa1df64798f057cb6",
+    "url": "/react-firebase/static/css/2.8fbeba8e.chunk.css"
+  },
+  {
+    "revision": "3e3b9de957cc15739c36",
+    "url": "/react-firebase/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "27cfa1df64798f057cb6",
+    "url": "/react-firebase/static/js/2.c989f035.chunk.js"
+  },
+  {
+    "revision": "b7b515ea063c721ace9d4afdae167dfb",
+    "url": "/react-firebase/static/js/2.c989f035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3b9de957cc15739c36",
+    "url": "/react-firebase/static/js/main.c8849912.chunk.js"
+  },
+  {
+    "revision": "5f0d71511b765dc9e784",
+    "url": "/react-firebase/static/js/runtime-main.3d52b7ab.js"
+  }
+]);
